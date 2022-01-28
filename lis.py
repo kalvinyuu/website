2f661befb7df5b80cb6b45c0a324be6610054719
Kalvin Yu <33174093+kalvinyuu@users.noreply.github.com>
2022-01-28 19:30:27 +0000

    added notes for each section

--- lis.py
+++ lis.py
@@ -6,13 +6,15 @@ Exp    = (Atom, List)
 Env    = dict             # A Scheme environment (defined below) 
                           # is a mapping of {variable: value}
                           
- #this turns strings of text into REPL parsable code or tokens. 
+ # this turns strings of text into REPL parsable code or tokens. 
 def  tokenise(chars: str) -> list:
     return chars.replace('(', ' (' ).replace(')', ' )').split
 
+# reads expression from a python string
 def parse(program: str) -> Exp:
     return read_from_tokens(tokenise(program))
 
+# reads expression from a sequence of tokens
 def read_from_tokens(tokens: list) -> Exp:
     if len(tokens) == 0:
         raise SyntaxError('unexpected EOF')
@@ -28,5 +30,11 @@ def read_from_tokens(tokens: list) -> Exp:
     else:
         return atom(token)
     
-    def atom(token: str) -> Atom:
-        
+# differentiates between numbers and sybols by testing atom
+# first it checks if int then float then concludes symbol.
+def atom(token: str) -> Atom:
+    try: return int(token)
+    except ValueError:
+        try: return float(token)
+        except ValueError:
+            return Symbol(token)        

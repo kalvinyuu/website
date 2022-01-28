edb2a31aa880b9066aa159cc90567e8649a233ab
Kalvin Yu <33174093+kalvinyuu@users.noreply.github.com>
2022-01-28 20:23:26 +0000

    done for today

--- lis.py
+++ lis.py
@@ -1,3 +1,6 @@
+import math
+import operator as op
+
 Symbol = str              # A symbol returns itself 
 Number = (int, float)    
 Atom   = (Symbol, Number)
@@ -7,15 +10,18 @@ Env    = dict             # A Scheme environment (defined below)
                           # is a mapping of {variable: value}
                           
  # this turns strings of text into REPL parsable code or tokens. 
-def  tokenise(chars: str) -> list:
+def tokenise(chars: str) -> list:
+    "Convert a string of characters into a list of tokens."
     return chars.replace('(', ' (' ).replace(')', ' )').split
 
 # reads expression from a python string
 def parse(program: str) -> Exp:
+    "Read a Scheme expression from a string."
     return read_from_tokens(tokenise(program))
 
 # reads expression from a sequence of tokens
 def read_from_tokens(tokens: list) -> Exp:
+    "Read an expression from a sequence of tokens."
     if len(tokens) == 0:
         raise SyntaxError('unexpected EOF')
     token = token.pop(0)
@@ -33,8 +39,84 @@ def read_from_tokens(tokens: list) -> Exp:
 # differentiates between numbers and sybols by testing atom
 # first it checks if int then float then concludes symbol.
 def atom(token: str) -> Atom:
+    "Numbers become numbers; every other token is a symbol."
     try: return int(token)
     except ValueError:
         try: return float(token)
         except ValueError:
             return Symbol(token)        
+
+# parser complete woo!
+
+# environment section?
+
+def standard_env() -> Env:
+    "An environment with some Scheme standard procedures."
+    env = Env()
+    env.update(vars(math)) # sin, cos, sqrt, pi, ...
+    env.update({
+        '+':op.add, '-':op.sub, '*':op.mul, '/':op.truediv, 
+        '>':op.gt, '<':op.lt, '>=':op.ge, '<=':op.le, '=':op.eq, 
+        'abs':     abs,
+        'append':  op.add,  
+        'apply':   lambda proc, args: proc(*args),
+        'begin':   lambda *x: x[-1],
+        'car':     lambda x: x[0],
+        'cdr':     lambda x: x[1:], 
+        'cons':    lambda x,y: [x] + y,
+        'eq?':     op.is_, 
+        'expt':    pow,
+        'equal?':  op.eq, 
+        'length':  len, 
+        'list':    lambda *x: List(x), 
+        'list?':   lambda x: isinstance(x, List), 
+        'map':     map,
+        'max':     max,
+        'min':     min,
+        'not':     op.not_,
+        'null?':   lambda x: x == [], 
+        'number?': lambda x: isinstance(x, Number),  
+		'print':   print,
+        'procedure?': callable,
+        'round':   round,
+        'symbol?': lambda x: isinstance(x, Symbol),
+    })
+    return env
+
+global_env = standard_env()
+
+# evaluates all objects
+
+def eval(x: Exp, env=global_env) -> Exp:
+    "Evaluate an expression in an environment."
+    if isinstance(x, Symbol):        # variable reference
+        return env[x]
+    elif isinstance(x, Number):      # constant number
+        return x                
+    elif x[0] == 'if':               # conditional
+        (_, test, conseq, alt) = x
+        exp = (conseq if eval(test, env) else alt)
+        return eval(exp, env)
+    elif x[0] == 'define':           # definition
+        (_, symbol, exp) = x
+        env[symbol] = eval(exp, env)
+    else:                            # procedure call
+        proc = eval(x[0], env)
+        args = [eval(arg, env) for arg in x[1:]]
+        return proc(*args)
+
+# makes repl interactive
+
+def repl(prompt='lis.py> '):
+    "A prompt-read-eval-print loop."
+    while True:
+        val = eval(parse(raw_input(prompt)))
+        if val is not None: 
+            print(schemestr(val))
+
+def schemestr(exp):
+    "Convert a Python object back into a Scheme-readable string."
+    if isinstance(exp, List):
+        return '(' + ' '.join(map(schemestr, exp)) + ')' 
+    else:
+        return str(exp)
